243a9e7a0182e1c9cd29bc84795c9a6d23ee1770
tsurowa <maria.tsurowa@gmail.com>
2023-03-20 15:11:00 +0200

    Update Card.jsx

--- collections/Card/Card.jsx
+++ collections/Card/Card.jsx
@@ -1,1 +1,30 @@
-// The Card to be exported goes here
+import { loadGetInitialProps } from "next/dist/shared/lib/utils";
+import Image from "next/image";
+
+import {
+  StyledTitle,
+  StyledDescription,
+  StyledIconContainer,
+  StyledCardContainer,
+  StyledTextContainer,
+} from "./elements";
+
+export const Card = ({ image, title, description, position, ...props }) => {
+  return (
+    <StyledCardContainer position={position}>
+      <StyledIconContainer>
+        <Image
+          layout="intrinsic"
+          src={image.src}
+          alt={image.alt}
+          width={image.width}
+          height={image.height}
+        />
+      </StyledIconContainer>
+      <StyledTextContainer>
+        <StyledTitle>{title}</StyledTitle>
+        <StyledDescription>{description}</StyledDescription>
+      </StyledTextContainer>
+    </StyledCardContainer>
+  );
+};
